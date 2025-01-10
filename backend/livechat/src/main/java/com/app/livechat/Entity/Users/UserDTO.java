76e257a837958960e9a92390036d6f723995ad47
AlexDev <ariverar151097@icloud.com>
2025-01-09 22:56:02 -0600

    dto  user for api requests

---
+++ backend/livechat/src/main/java/com/app/livechat/Entity/Users/UserDTO.java
@@ -0,0 +1,17 @@
+package com.app.livechat.Entity.Users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+
+    private String email;
+    private String password;
+
+}

adcc1c0d2215e8e8e0ec5b32d1604682157ec488
AlexDev <ariverar151097@icloud.com>
2025-01-09 22:55:27 -0600

    user entity

---
+++ backend/livechat/src/main/java/com/app/livechat/Entity/Users/User.java
@@ -0,0 +1,46 @@
+package com.app.livechat.Entity.Users;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+import com.app.livechat.Entity.Conversations.Conversation;
+
+
+@Entity(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Double id;
+    private String email;
+    private String password;
+    private String name;
+
+
+    @ManyToMany
+    private List<Conversation> conversations;
+
+
+
+
+
+    public User(String email, String password){
+        this.email = email; 
+        this.password = password;   
+    }
+
+
+}
